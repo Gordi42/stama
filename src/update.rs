158f120faeffc3d9bd6dd5e5a82f9152f0d2225f
Gordi42 <silvano98@hotmail.de>
2024-03-30 00:03:39 +0100

    add keybindings for message

--- src/update.rs
+++ src/update.rs
@@ -14,21 +14,12 @@ pub fn update(app: &mut App, key_event: KeyEvent) {
     };
 
     // pass the key event to the app menus
-    // let input_functions = [
-    //     || app.message.input(&mut app.action, key_event),
-    //     || app.job_overview.input(&mut app.action, key_event),
-    // ];
-    //
-    // let mut input_handled = false;
-    // for input_function in input_functions.iter() {
-    //     if input_handled {
-    //         break;
-    //     }
-    //     input_handled = input_function();
-    // }
 
-    // app.message.input(&mut app.action, key_event);
-    // app.job_overview.input(&mut app.action, key_event);
+    let mut input_handled = false;
+    if !input_handled {
+        input_handled = app.message.input(&mut app.action, key_event);};
+    if !input_handled {
+        app.job_overview.input(&mut app.action, key_event);};
 
 
     app.handle_action();
