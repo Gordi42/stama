ca2f9a8b22323edaaa5f5faf1a021f00872c5c88
Gordi42 <silvano98@hotmail.de>
2024-04-04 16:56:32 +0200

    add help text when no --output-file is given

--- src/write_output.rs
+++ src/write_output.rs
@@ -11,5 +11,10 @@ pub fn write_output_file(content: &str) {
 
     if let Some(output_file) = matches.get_one::<String>("output-file") {
         std::fs::write(output_file, content).expect("Unable to write file");
+    } else {
+        println!("It seems that you didn't provide an output file path. The content will be printed to the console instead:");
+        println!("{}", content);
+        println!("For more information on how to execute the command automatically, please refer to the documentation:");
+        println!("GitHub:   https://github.com/Gordi42/stama");
     }
 }
